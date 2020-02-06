ee895cecb4035b793185632b69cb06a71b96f092
Sarim-K <shktv03@gmail.com>
2020-02-06 18:02:52 +0000

    added button_info class instead of using 2d array added exponent button

--- calculator.py
+++ calculator.py
@@ -3,6 +3,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QKeySequence, QIcon, QFont, QColor
 from PyQt5 import QtCore
 
+class button_info:
+    def __init__(self,button_text,button_function,xpos,ypos):
+        self.button_text = button_text
+        self.button_function = button_function
+        self.xpos = xpos
+        self.ypos = ypos
+        
 class main_window(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -27,14 +34,19 @@ class main_widget(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.to_calculate = ""
         self.repeat_operator_list = ["+", "/", "*"]
         self.operator_list = ["+", "/", "*","-"]
         self.number_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-        self.button_info_list = [ [0,0,0] , [1,1,0] , [2,2,0] , [3,0,1] , [4,1,1] , [5,2,1] , [6,0,2] , [7,1,2], [8,2,2] , [9,0,3] , ["+",3,0], ["-",3,1] , ["/",3,2] , ["*",3,3] , ["(",1,3] , [")",2,3] ] #button text, x pos, y pos
-        self.button_dict = {}
+        #button text, function, x pos, y pos
+        self.button_info_list = [ button_info("0","0",0,0) , button_info("1","1",1,0) , button_info("2","2",2,0) , button_info("+","+",3,0) , 
+                                  button_info("3","3",0,1) , button_info("4","4",1,1) , button_info("5","5",2,1) , button_info("-","-",3,1) ,
+                                  button_info("6","6",0,2) , button_info("7","7",1,2) , button_info("8","8",2,2) , button_info("/","/",3,2) ,
+                                  button_info("9","9",0,3) , button_info("(","(",1,3) , button_info(")",")",2,3) , button_info("x","*",3,3) ,
+                                                                                        button_info("^","**",2,4)                           ]
 
-        self.to_calculate = ""
+        self.button_dict = {} #to be populated by button objects
 
         self.initUI()
 
@@ -42,16 +54,16 @@ class main_widget(QWidget):
         grid_layout = QGridLayout()
         self.setLayout(grid_layout)
 
-        # creates all number + operator buttons
-        for item in self.button_info_list:
-            self.number_button = QPushButton(str(item[0]))
+        # creates all buttons in button_info_list, adds to button_dict
+        for button_info_obj in self.button_info_list:
+            self.number_button = QPushButton(button_info_obj.button_text)
             self.number_button.setFixedSize(50,50)
-            self.number_button.setShortcut(f"{item[0]}")
+            self.number_button.setShortcut(button_info_obj.button_text)
             self.number_button.setStyleSheet("background-color: white")
             self.number_button.clicked.connect(self.on_num_operator_clicked)
-            grid_layout.addWidget(self.number_button, item[2],item[1]) 
+            grid_layout.addWidget(self.number_button, button_info_obj.ypos, button_info_obj.xpos) 
             
-            self.button_dict[str(item[0])] = self.number_button
+            self.button_dict[button_info_obj.button_text] = self.number_button
             del self.number_button
         
         # enter button
@@ -67,19 +79,27 @@ class main_widget(QWidget):
         font = QFont("Arial", 12, QFont.Bold)
         self.output_label = QLabel("")
         self.output_label.setFont(font)
-        grid_layout.addWidget(self.output_label,4,0,4,3)
+        grid_layout.addWidget(self.output_label,4,0,4,2)
         
     def on_enter(self):
         try:
             self.multiply_unprefixed_brackets()
             self.multiply_unsuffixed_brackets()
-            self.output_label.setText(str(eval(self.to_calculate)))
+            self.check_first_char()
             self.to_calculate = str(eval(self.to_calculate))
-        except:
+            self.output_label.setText(str(self.to_calculate))
+
+        except Exception as e:
+            print(e)
             self.output_label.setText("ERROR.")
 
     def on_num_operator_clicked(self):
-        self.to_calculate += self.sender().text()
+        try: #searches for corresponding object info, and adds its function to self.to_calculate
+            for self.num_operator in self.button_info_list:
+                if str(self.num_operator.button_text) == str(self.sender().text()):
+                    self.to_calculate += str(self.num_operator.button_function)
+        except Exception as e:
+            print(e)
 
         self.check_prev_char()
         self.check_first_char()
@@ -106,30 +126,42 @@ class main_widget(QWidget):
 
     # used to make sure the first input isnt an operator, excluding "-"
     def check_first_char(self):
-        if self.to_calculate[0] in self.repeat_operator_list and len(self.to_calculate) <= 1:
-            self.to_calculate = ""
+        for _ in range(len(self.to_calculate)):
+            if self.to_calculate[0] in self.repeat_operator_list and len(self.to_calculate) <= 1:
+                self.to_calculate = self.to_calculate[1:]
 
     def multiply_unprefixed_brackets(self):
         self.indexes = [i for i, letter in enumerate(self.to_calculate) if letter == "("]
+        try:
+            self.indexes.remove(0)
+        except:
+            pass
+
         if len(self.to_calculate) > 1:
             for self.index in self.indexes:
-                if self.to_calculate[self.index-1] not in self.operator_list:
-                    self.temp_array = list(self.to_calculate)
-                    self.temp_array.insert(self.index, "*")
-                    self.to_calculate = ""
-                    for char in self.temp_array:
-                        self.to_calculate += char
+                try:
+                    if self.to_calculate[self.index-1] not in self.operator_list:
+                        self.temp_array = list(self.to_calculate)
+                        self.temp_array.insert(self.index, "*")
+                        self.to_calculate = ""
+                        for char in self.temp_array:
+                            self.to_calculate += char
+                except Exception as e:
+                    print(e)
 
     def multiply_unsuffixed_brackets(self):
         self.indexes = [i for i, letter in enumerate(self.to_calculate) if letter == ")"]
         if len(self.to_calculate) > 1:
             for self.index in self.indexes:
-                if self.to_calculate[self.index+1] not in self.operator_list:
-                    self.temp_array = list(self.to_calculate)
-                    self.temp_array.insert(self.index+1, "*")
-                    self.to_calculate = ""
-                    for char in self.temp_array:
-                        self.to_calculate += char
+                try:
+                    if self.to_calculate[self.index+1] not in self.operator_list:
+                        self.temp_array = list(self.to_calculate)
+                        self.temp_array.insert(self.index+1, "*")
+                        self.to_calculate = ""
+                        for char in self.temp_array:
+                            self.to_calculate += char
+                except Exception as e:
+                    print(e)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
